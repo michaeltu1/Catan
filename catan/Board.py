fe5724f781a9b9029b66e24771e33dc4673c5e61
Michael <michaeltu22@gmail.com>
2018-06-28 22:40:32 -0700

    added resource types

--- catan/Board.py
+++ catan/Board.py
@@ -1,3 +1,5 @@
+import random
+
 # TODO(mtu): figure out how imports work
 from catan.config.Config import Config
 from catan.Utils import *
@@ -21,9 +23,20 @@ class Board:
         # Retrieve high and low probability dice roll numbers
         common_rolls, uncommon_rolls = high_low_rolls(self.distribution)
 
+        self.tile_ids = [x for x in range(19)]
+        self.resource_types = ["Wheat"] * 4 + ["Sheep"] * 4 + ["Ore"] * 3 + \
+                              ["Clay"] * 3 + ["Wood"] * 4 + ["Desert"]
+
+    """
+    def generate_random_board(self):
+        random.shuffle(self.tile_ids)
+        random.shuffle(resource_types)
+        zip(self.tile_ids, self.resource_types)
+    """
+
     def __str__(self):
         # TODO(mtu): print the board
-        raise NotImplementedError
+        return "blah"
 
 
 if __name__ == '__main__':
--- catan/Utils.py
+++ catan/Utils.py
@@ -6,7 +6,7 @@ def high_low_rolls(distribution):
     Returns the lists of high and low probability rolls
     """
     high_probability_rolls = np.argwhere(np.max(distribution) == distribution).reshape(1, -1)
-    high = high_probability_rolls.to
+    high = high_probability_rolls.tolist()[0]
     low = [roll for roll in list(range(2, 13)) if roll not in high]
     return high, low
 
