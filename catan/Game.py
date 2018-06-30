2b656752e725f769dbe1b3154c6ee953185754f2
Eric Pan <yup27@cs.washington.edu>
2018-06-30 01:24:45 -0700

    Initialized inventory information, having trouble with ports

--- catan/Game.py
+++ catan/Game.py
@@ -2,3 +2,9 @@ class Game:
     def __init__(self):
         self.intersections = None
         self.unbuildable = set()
+        
+        resource_cards = {"Wheat" : 19, "Sheep" : 19, "Ore" : 19, "Clay" : 19, "Wood" : 19}
+        dev_cards = {"Knight" : 14, "Victory Point" : 5, "Road Building" : 2, "Year of Plenty" : 2, "Monopoly" : 2}
+        #ports = {
+
+        self.game_resources = Inventory(resource_cards, dev_cards, True, True)
--- catan/Inventory.py
+++ catan/Inventory.py
@@ -1,13 +1,18 @@
 class Inventory:
     """
     Game.py should initialize the dictionary of resource and dev cards
-    should map resource type (key) to number of that type of card (value),
+    should map resource type (key) to number of that type of card (value), 
+    also initialize ports
     and initialize longest_road and largest_army to true;
-    a player by default has no cards
+    a player by default has no cards and no ports
+
+    TODO: Figure out how to represents ports, location of port comes from intersection,
+    but need to also know what type of port and quantity.
     """
-    def __init__(self, resource_cards={}, dev_cards={}, longest_road=False, largest_army=False):
+    def __init__(self, resource_cards={}, dev_cards={}, ports=set() longest_road=False, largest_army=False):
         self.resource_cards = resource_cards
         self.dev_cards = dev_cards
+        self.ports = ports
         self.longest_road = longest_road
         self.largest_army = largest_army
 
