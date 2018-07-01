900df792b4ee5ec0d4cc1929cf0a58a536090643
Eric Pan <yup27@cs.washington.edu>
2018-06-30 17:49:02 -0700

    Implemented constructor for Player class, started on action

--- catan/Player.py
+++ catan/Player.py
@@ -1,8 +1,8 @@
-
+from catan.Inventory import *
 
 # originally in intersection but moved to here
 # need to update board or game with updated intersection attributes (has_settlement or has_city)
-def build(self):
+def build_settlement(self):
     if self.intersect_ID not in Game.unbuildable:
         Game.unbuildable.add(self.intersect_ID)
         Game.unbuildable.add((self.intersect_ID.index(0) + 20,
@@ -11,3 +11,15 @@ def build(self):
                               self.intersect_ID.index(1) - 7, self.intersect_ID.index(2)))
         Game.unbuildable.add((self.intersect_ID.index(0),
                               self.intersect_ID.index(1), self.intersect_ID.index(2) - 13))
+
+class Player:
+
+    def __init__(self, player_id, knights_played=0):
+        self.player_id = player_id
+        self.knights_played = knights_played
+        self.backpack = Backpack(5, 4, 16)
+
+    def action(self, input_string):
+        if input_string == "build road":
+
+
