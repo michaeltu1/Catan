f10dcf100e0b386637f8df1f227d840c9e2d0d53
Eric Pan <yup27@cs.washington.edu>
2018-07-03 05:42:54 -0700

    Removed action from player

--- catan/Player.py
+++ catan/Player.py
@@ -1,18 +1,10 @@
 from catan.Inventory import *
-from catan.Game import *
 # Maybe don't need to import everything
 
 class Player:
-    """
-    Player can have a position in {North, East, South, West}
-    """
+    
     def __init__(self, player_id, knights_played=0):
         self.player_id = player_id
         self.knights_played = knights_played
         self.backpack = Backpack(5, 4, 16)
-
-    def action(self, input_string):
-        if input_string == "build road":
-
-            Game.build_road(self.player_id)            
 
