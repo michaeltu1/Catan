ceccc06acbae2b23a6e8bde4ebd66907bd3380bf
Eric Pan <yup27@cs.washington.edu>
2018-07-03 05:45:37 -0700

    Added user input/output

--- catan/Game.py
+++ catan/Game.py
@@ -10,24 +10,50 @@ class Game:
         dev_cards = {"Knight" : 14, "Victory Point" : 5, "Road Building" : 2, "Year of Plenty" : 2, "Monopoly" : 2}
 
         self.game_resources = Inventory(resource_cards, dev_cards)
+        self.game_over = False
 
 
     def start_game(self, num_players):
         self.player_list = []
-        max_roll = (0, 0)   # (player_id, roll_num)
+        player_ids = []
 
-        # Create players, player_id starts at 1 and decide who goes first, todo: tie breaker?
+        # Create players, player_id starts at 1 
         for number in range(num_players):
             player = Player(number)
-            r = roll_dice()
-            max_roll = (number, r) if r > max_roll[1] else max_roll
             self.player_list.append(player) 
+            player_ids.append(number)
         
-        for player_id in range(max_roll[0], max_roll[0] + num_players):
-            player_id %= (num_players + 1)
-            # response = where would you like to build? check if possible
-            build_settlement(player_id, response)
-            build_road(player_id, response)
+        # Decide who goes first
+        player_first = _starter(player_ids)
+
+        
+        # First round of choosing settlements and roads
+        for player_id in range(player_first, player_first + num_players):
+            player_id %= num_players
+            _ux(player_id, "build settlement")
+            _ux(player_id, "build road")
+
+        # Second round of choosing settlements and roads
+        for player_id in reversed(range(player_first, player_first + num_players)):
+            player_id %= num_players
+            _ux(player_id, "build settlement")
+
+            # Get the rolls num associated with the tiles that the settlement are touching
+            # and give those resources to the player
+            for roll_num in self.player_list[player_id].backpack.rolls.keys():
+                distribute_resources(player_id, roll_num)
+            _ux(player_id, "build road")
+        
+        while !self.game_over:
+            for player_id in range(player_first, player_first + num_players):
+                done = False
+                while !done:
+                    action = _ux(player_id, "ask for action")
+                    done = _ux(player_id, action)
+
+                if self.player_list[player_id].backpack.victory_points == 10:
+                    self.game_over = True
+                    print("Player %s has won the game" % player_id)
 
 #b = game.player_list[0].backpack
 #print(b), can't print out actual information for some reason
@@ -40,6 +66,73 @@ def roll_dice():
     return rolls[0] + rolls[1]
 
 """
+Decides which player goes first
+returns the player_id that wins
+"""
+def _starter(player_ids):
+    highest_roll = 0
+    player_rolls = {}
+    while len(player_ids) > 1:
+        for player in player_ids:
+            r = roll_dice()
+            player_rolls[player] = r
+            highest_roll = r if r > highest_roll else highest roll
+        player_ids = [k for k, v in player_rolls.items() if v == highest_roll]
+        highest_roll = 0
+        player_rolls.clear()
+    return player_ids[0]
+
+"""
+Takes care of user experience; given a player and action
+perform the given action, returns True if done otherwise False or an action
+"""
+def _ux(player_id, action):
+    actions = ["build road", "build settlement", "build city", "buy dev card", "use dev card", "trade", "done"]
+    trades = ["player", "port", "4:1"]
+    ports = ["wheat", "sheep", "ore", "clay", "wood", "3:1"]
+
+    if action == "ask for action":
+        response = input("Player %s, what would you like to do? " % player_id)
+        while response.lower() not in actions:
+            response = input("That's not a valid action, please choose one from the following: %s" % actions)
+        return response
+
+    else if action == "done":
+        return True
+    else if action.split(" ")[0] == "build":
+        obj = action.split(" ")[1]
+        build_location = input("Player %s, where would you like to build your %s ? " % (player_id, obj))
+        if obj == "settlement":            
+            while !build_settlement(player_id, build_location):
+                build_location = input("Sorry, you cannot build there, please choose another location: ")
+        else if obj == "city":
+            while !build_city(player_id, build_location):
+                build_location = input("Sorry, you cannot build there, please choose another location: ")
+        else if obj == "road":
+            while !build_road(player_id, build_location):
+                build_location = input("Sorry, you cannot build there, please choose another location: ")
+
+    else if action == "buy dev card":
+        buy_dev_card(player_id)
+
+    else if action == "use dev card":
+        use_dev_card(player_id)
+
+    else if action == "trade": #TODO
+        response = input("What type of trade would you like to do? ")
+        while response.lower() not in trades:
+            response = input("That's not a valid trade, please choose from the following: %s" % trades)
+        if response == "player":
+            response = input("")
+        else if response == "port":
+            response = input("Which port would you like to use? ")
+            while response.lower() not in ports:
+                response = input("That's not a valid port, please choose from the following: %s" % ports)            
+        else if response == "4:1":
+            response = input("")
+    return False
+
+"""
 Given a player and roll_num, distribute resources according to what tiles the player owns
 """
 def distribute_resources(player_id, roll_num):
@@ -47,6 +140,7 @@ def distribute_resources(player_id, roll_num):
 
 """
 Take in a player_id and attempt to build a road at an edge tuple (x, y)
+Need to update player's backpack's num_roads, roads
 returns true if successful, otherwise false.
 """
 def build_road(player_id, edge):
@@ -54,6 +148,7 @@ def build_road(player_id, edge):
 
 """
 Take in a player_id and attempt to build a settlement at an intersection tuple (x, y, z)
+Need to update player's backpack's num_settlements, victory_points, rolls, tiles, and ports if applicable
 returns true if successful, otherwise false.
 """
 def build_settlement(player_id, intersection):
@@ -83,6 +178,16 @@ returns true if successful and will add the dev card to player's backpack, other
 """
 def buy_dev_card(player_id):
     return False
+
+"""
+Take in a player_id and use a dev card
+make sure that the card wasn't bought in the same turn
+returns true if used successfully, otherwise returns false
+"""
+
+def use_dev_card(player_id):
+    return False
+
 
 """
 Take in a player_id and two resource types;
