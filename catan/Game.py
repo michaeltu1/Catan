8272da2649d9b8c19e811892a39cfe361a43be63
Eric Pan <yup27@cs.washington.edu>
2018-07-03 20:24:49 -0700

    moved ux to game class

--- catan/Game.py
+++ catan/Game.py
@@ -30,29 +30,29 @@ class Game:
         # First round of choosing settlements and roads
         for player_id in range(player_first, player_first + num_players):
             player_id %= num_players
-            _ux(player_id, "build settlement")
-            _ux(player_id, "build road")
+            self.__ux(player_id, "build settlement")
+            self.__ux(player_id, "build road")
 
         # Second round of choosing settlements and roads
         for player_id in reversed(range(player_first, player_first + num_players)):
             player_id %= num_players
-            _ux(player_id, "build settlement")
+            self.__ux(player_id, "build settlement")
 
             # Get the rolls num associated with the tiles that the settlement are touching
             # and give those resources to the player
             for roll_num in self.player_list[player_id].backpack.rolls.keys():
-                distribute_resources(player_id, roll_num)
-            _ux(player_id, "build road")
+                self.distribute_resources(player_id, roll_num)
+            self.__ux(player_id, "build road")
         
         while not self.game_over:
             for player_id in range(player_first, player_first + num_players):
                 r = roll_dice()
-                distribute_resources(player_id, r)
+                self.distribute_resources(player_id, r)
 
                 done = False
                 while not done:
-                    action = _ux(player_id, "ask for action")
-                    done = _ux(player_id, action)
+                    action = self.__ux(player_id, "ask for action")
+                    done = self.__ux(player_id, action)
 
                 if self.player_list[player_id].backpack.victory_points == 10:
                     self.game_over = True
@@ -124,6 +124,56 @@ class Game:
     def trade(self, player_id, resource_1, resource_2, other_player_id=-1, port_name=None):
         return True
 
+    """
+    Takes care of user experience; given a player and action
+    perform the given action, returns True if done otherwise False or an action
+    """
+    def __ux(self, player_id, action):
+        actions = ["build road", "build settlement", "build city", "buy dev card", "use dev card", "trade", "done"]
+        trades = ["player", "port", "4:1"]
+        ports = ["wheat", "sheep", "ore", "clay", "wood", "3:1"]
+
+        if action == "ask for action":
+            response = input("Player %s, what would you like to do? " % player_id)
+            while response.lower() not in actions:
+                response = input("That's not a valid action, please choose one from the following: %s" % actions)
+            return response
+
+        elif action == "done":
+            return True
+        elif action.split(" ")[0] == "build":
+            obj = action.split(" ")[1]
+            build_location = input("Player %s, where would you like to build your %s ? " % (player_id, obj))
+            if obj == "settlement":            
+                while not self.build_settlement(player_id, build_location):
+                    build_location = input("Sorry, you cannot build there, please choose another location: ")
+            elif obj == "city":
+                while not self.build_city(player_id, build_location):
+                    build_location = input("Sorry, you cannot build there, please choose another location: ")
+            elif obj == "road":
+                while not self.build_road(player_id, build_location):
+                    build_location = input("Sorry, you cannot build there, please choose another location: ")
+
+        elif action == "buy dev card":
+            self.buy_dev_card(player_id)
+
+        elif action == "use dev card":
+            self.use_dev_card(player_id)
+
+        elif action == "trade": #TODO
+            response = input("What type of trade would you like to do? ")
+            while response.lower() not in trades:
+                response = input("That's not a valid trade, please choose from the following: %s" % trades)
+            if response == "player":
+                response = input("")
+            elif response == "port":
+                response = input("Which port would you like to use? ")
+                while response.lower() not in ports:
+                    response = input("That's not a valid port, please choose from the following: %s" % ports)            
+            elif response == "4:1":
+                response = input("")
+        return False
+
 #b = game.player_list[0].backpack
 #print(b), can't print out actual information for some reason
 
@@ -151,55 +201,6 @@ def _starter(player_ids):
         player_rolls.clear()
     return player_ids[0]
 
-"""
-Takes care of user experience; given a player and action
-perform the given action, returns True if done otherwise False or an action
-"""
-def _ux(player_id, action):
-    actions = ["build road", "build settlement", "build city", "buy dev card", "use dev card", "trade", "done"]
-    trades = ["player", "port", "4:1"]
-    ports = ["wheat", "sheep", "ore", "clay", "wood", "3:1"]
-
-    if action == "ask for action":
-        response = input("Player %s, what would you like to do? " % player_id)
-        while response.lower() not in actions:
-            response = input("That's not a valid action, please choose one from the following: %s" % actions)
-        return response
-
-    elif action == "done":
-        return True
-    elif action.split(" ")[0] == "build":
-        obj = action.split(" ")[1]
-        build_location = input("Player %s, where would you like to build your %s ? " % (player_id, obj))
-        if obj == "settlement":            
-            while not build_settlement(player_id, build_location):
-                build_location = input("Sorry, you cannot build there, please choose another location: ")
-        elif obj == "city":
-            while not build_city(player_id, build_location):
-                build_location = input("Sorry, you cannot build there, please choose another location: ")
-        elif obj == "road":
-            while not build_road(player_id, build_location):
-                build_location = input("Sorry, you cannot build there, please choose another location: ")
-
-    elif action == "buy dev card":
-        buy_dev_card(player_id)
-
-    elif action == "use dev card":
-        use_dev_card(player_id)
-
-    elif action == "trade": #TODO
-        response = input("What type of trade would you like to do? ")
-        while response.lower() not in trades:
-            response = input("That's not a valid trade, please choose from the following: %s" % trades)
-        if response == "player":
-            response = input("")
-        elif response == "port":
-            response = input("Which port would you like to use? ")
-            while response.lower() not in ports:
-                response = input("That's not a valid port, please choose from the following: %s" % ports)            
-        elif response == "4:1":
-            response = input("")
-    return False
 
 
 
