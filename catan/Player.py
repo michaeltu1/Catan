7b55bbe2ff48794f4f0ea4e88d4c5c3896d898b6
Eric Pan <yup27@cs.washington.edu>
2018-07-06 12:21:52 -0700

    Added dev card used (this turn) attribute

--- catan/Player.py
+++ catan/Player.py
@@ -4,9 +4,10 @@ from catan.Inventory import *
 
 class Player:
     
-    def __init__(self, player_id, knights_played=0):
+    def __init__(self, player_id, knights_played=0, dev_cards_used=False):
         self.player_id = player_id
         self.knights_played = knights_played
+        self.dev_cards_used = dev_cards_used
         self.backpack = Backpack(5, 4, 16)
     
     def __str__(self):
