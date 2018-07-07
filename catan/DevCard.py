202927115b79b4ab5f0d2bb36ff9fa4ba96684e3
Eric Pan <yup27@cs.washington.edu>
2018-07-07 04:42:06 -0700

    Added attribute to make dev card usage easier

--- catan/DevCard.py
+++ catan/DevCard.py
@@ -2,15 +2,15 @@ class DevCard:
     """
     Needed to keep track of when a player can use their dev card
     """
-    def __init__(self, card_type, description, can_use=False, used=False):
+    def __init__(self, card_type, description, turn_bought=0, used=False):
         self.card_type = card_type
         self.description = description
-        self.can_use = can_use
+        self.turn_bought = turn_bought
         self.used = used
 
     def __str__(self):
-        return "Card type: %s, Description: %s, Can_use: %s, Used: %s" % \
-               (self.card_type, self.description, self.can_use, self.used)
+        return "Card type: %s, Description: %s, Turn bought: %s, Used: %s" % \
+               (self.card_type, self.description, self.turn_bought, self.used)
 
     def __repr__(self):
-        return "DevCard(%s, %s, %s, %s)" % (self.card_type, self.description, self.can_use, self.used)
+        return "DevCard(%s, %s, %s, %s)" % (self.card_type, self.description, self.turn_bought, self.used)
--- catan/Player.py
+++ catan/Player.py
@@ -3,16 +3,17 @@ from catan.Inventory import *
 
 
 class Player:
-    
-    def __init__(self, player_id, knights_played=0, dev_cards_used=False):
+    # Turns start when the dice is rolled
+    def __init__(self, player_id, turn_number=1, knights_played=0, dev_cards_used=False):
         self.player_id = player_id
+        self.turn_number = turn_number
         self.knights_played = knights_played
         self.dev_cards_used = dev_cards_used
         self.backpack = Backpack(5, 4, 16)
     
     def __str__(self):
-        return "Player id: %s, knights played: %s, backpack: %s" % \
-                (self.player_id, self.knights_played, self.backpack)
+        return "Player id: %s, Turn number: %s, knights played: %s, backpack: %s" % \
+                (self.player_id, self.turn_number, self.knights_played, self.backpack)
 
     def __repr__(self):
-        return "Player(%s, %s, %s)" % (self.player_id, self.knights_played, self.backpack)
+        return "Player(%s, %s, %s, %s)" % (self.player_id, self.turn_number, self.knights_played, self.backpack)
